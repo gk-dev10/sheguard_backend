36594cb5144a9c932bf717bb27d3fa79c4761156
Yogesh <yogeshkumar.n1234@gmail.com>
2026-02-17 19:39:01 +0530

    fix: prod level db init

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,21 +2,51 @@ package db
 
 import (
 	"context"
+	"errors"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var Pool *pgxpool.Pool
 
-func Init() error {
+func Init(ctx context.Context) error {
 	url := os.Getenv("SUPABASE_DB_URL")
+	if url == "" {
+		return errors.New("SUPABASE_DB_URL is not set")
+	}
 
-	pool, err := pgxpool.New(context.Background(), url)
+	cfg, err := pgxpool.ParseConfig(url)
 	if err != nil {
+		return err
+	}
+
+	// pool tuning - Prod level
+	cfg.MaxConns = 10
+	cfg.MinConns = 2
+	cfg.MaxConnLifetime = time.Hour
+	cfg.MaxConnIdleTime = 30 * time.Minute
+	cfg.HealthCheckPeriod = 1 * time.Minute
+
+	pool, err := pgxpool.NewWithConfig(ctx, cfg)
+	if err != nil {
+		return err
+	}
+	ctxPing, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := pool.Ping(ctxPing); err != nil {
 		return err
 	}
 
 	Pool = pool
 	return nil
+}
+
+// to shut down the pool.
+func Close() {
+	if Pool != nil {
+		Pool.Close()
+	}
 }
