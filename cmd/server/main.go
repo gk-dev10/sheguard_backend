1c7aad07177f1222e1f9d4525a41dd656eb886e5
Yogesh <yogeshkumar.n1234@gmail.com>
2026-02-17 19:49:58 +0530

    fix: init db in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
+	"github.com/gk-dev10/sheguard_backend/internal/db"
 	"github.com/gk-dev10/sheguard_backend/internal/routes"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 func Test(c echo.Context) error {
@@ -17,6 +20,12 @@ func Test(c echo.Context) error {
 func main() {
 	e := echo.New()
 	godotenv.Load()
+	ctx := context.Background()
+	err := db.Init(ctx)
+	if err!= nil{
+		log.Fatal(err) //to end the program if db is not connected
+	}
+
 	//Test route
 	e.GET("/", Test)
 
